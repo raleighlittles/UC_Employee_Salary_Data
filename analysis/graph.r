081e9866a932d3d1f58919f2e16eef1b24a4bc41
Raleigh Littles III <raleighlittles@gmail.com>
2023-12-28 13:29:20 -0800

    added 2022 graph

--- analysis/graph.r
+++ analysis/graph.r
@@ -1,8 +1,8 @@
 library(ggplot2)
 library(ggstatsplot)
 
-all_uni_2021_data <- read.csv(file = "/home/raleigh/github/UC_Employee_Salary_Data/data/parsed/UCOP_Data_2021.csv", stringsAsFactors = TRUE)
+all_uni_2022_data <- read.csv(file = "../data/parsed/UCOP_Data_2022.csv", stringsAsFactors = TRUE)
 
-ggbetweenstats(data=all_uni_2021_data, x=location, y=gross.pay, outlier.tagging = TRUE, outlier.label = title, pairwise.comparisons = FALSE, title = "2021 Salary by UC campus", results.subtitle = FALSE, k = 0)
+ggbetweenstats(data=all_uni_2022_data, x=location, y=gross.pay, outlier.tagging = TRUE, outlier.label = title, pairwise.comparisons = FALSE, title = "2022 Salary by UC campus", results.subtitle = FALSE, k = 0)
 
-ggsave("2021_data.svg", plot = last_plot(), device = "svg", dpi = "retina")
+ggsave("2022_data.svg", plot = last_plot(), device = "svg", dpi = "retina")
