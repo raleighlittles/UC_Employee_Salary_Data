f6abea0d73e5b9b46d7813727e02db61c5fe48a7
Raleigh Littles <raleighlittles@gmail.com>
2024-10-04 00:41:40 -0700

    added analysis graphs

--- analysis/graph.r
+++ analysis/graph.r
@@ -1,8 +1,9 @@
 library(ggplot2)
 library(ggstatsplot)
 
-all_uni_2022_data <- read.csv(file = "../data/parsed/UCOP_Data_2022.csv", stringsAsFactors = TRUE)
+all_uni_2023_data <- read.csv(file = "../data/UCOP_Data_2023.csv", stringsAsFactors = TRUE)
 
-ggbetweenstats(data=all_uni_2022_data, x=location, y=gross.pay, outlier.tagging = TRUE, outlier.label = title, pairwise.comparisons = FALSE, title = "2022 Salary by UC campus", results.subtitle = FALSE, k = 0)
+ggbetweenstats(data=all_uni_2023_data, x=location, y=gross.pay, outlier.tagging = TRUE, outlier.label = title, pairwise.comparisons = FALSE, title = "2023 Salary by UC campus", results.subtitle = FALSE, k = 0)
 
-ggsave("2022_data.svg", plot = last_plot(), device = "svg", dpi = "retina")
+ggsave("2023_data.svg", plot = last_plot(), device = "svg", dpi = "retina")
+
